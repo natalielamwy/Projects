bc9e19e6902a77c10d910fcbbf1b6454ca2917f8
natalielamwy <natalielam.wy@gmail.com>
2025-07-02 00:00:11 -0700

    Added stuff to galaxy redshift project notebook

--- redshift_project/scripts/helper_functions.py
+++ redshift_project/scripts/helper_functions.py
@@ -1,4 +1,6 @@
 from astropy.table import Table
+import numpy as np
+    
 
 pretty_plot_params = {'font.size': 18, 
                      'text.usetex': True, 
@@ -18,20 +20,52 @@ pretty_plot_params = {'font.size': 18,
                      'xtick.minor.visible': True,
                      'ytick.minor.visible': True}
 
-def read_spectrum(filename):
+
+
+def get_continuum_mask(wave, wave_range, buffer=20):
     """
-    Reads a FITS file from SDSS and returns the spectrum data as an Astropy Table.
+    Function to get a boolean mask for the continuum regions based on the given wavelength range.
     
     Parameters:
-    filename (str): The path to the FITS file.
+    wave (array-like): Array of wavelength values.
+    wave_range (list of tuples): List of wavelength ranges to exclude from the continuum.
+    buffer (float): Buffer region around each wavelength range to exclude.
     
     Returns:
-    astropy.table.Table: The spectrum data.
+    np.ndarray: Boolean mask where True indicates continuum regions.
     """
+
+    mask = (wave >= wave_range[0]) & (wave < wave_range[1])
+    mask = ~mask & ((wave >= wave_range[0] - buffer) & 
+                    (wave < wave_range[1] + buffer))
+    return mask
+
+def subtract_continuum(wave, flux, wave_range=None, buffer=20):
+    """
+    Function to subtract the continuum from a spectrum.
     
-    t = Table.read(filename, hdu='COADD')
+    Parameters:
+    wave (array-like): Array of wavelength values.
+    flux (array-like): Array of flux values.
+    wave_range (list of tuples): List of wavelength ranges to exclude from the continuum.
+    buffer (float): Buffer region around each wavelength range to exclude.
+    
+    Returns:
+    tuple: Continuum-subtracted flux and the fitted continuum.
+    """
 
-    wavelength = 10**t['loglam']
-    flux = t['flux']
-
-    return wavelength, flux
+    if wave_range is None:
+        wave_range = [4980, 5020]
+    
+    mask = np.ones_like(wave, dtype=bool)
+    for wr in wave_range:
+        mask &= get_continuum_mask(wave, wr, buffer=buffer)
+    
+    # Fit a 3rd degree polynomial to the continuum regions
+    p_coeff = np.polyfit(wave[mask], flux[mask], deg=3)
+    continuum = np.polyval(p_coeff, wave)
+    
+    # Subtract the fitted continuum from the original flux
+    flux_cont_subtracted = flux - continuum
+    
+    return flux_cont_subtracted, continuum
